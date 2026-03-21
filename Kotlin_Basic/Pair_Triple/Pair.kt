0d64ca3f0fc38ce2344656f65794f145c79c089a
Siddhartha-4091 <gs092502@gmail.com>
2026-03-21 14:22:51 +0530

    Second

---
+++ Kotlin_Basic/Pair_Triple/Pair.kt
@@ -0,0 +1,10 @@
+package Kotlin_Basic.Pair_Triple
+
+fun main() {
+
+    val (a,b) = Pair("A",1)
+    println("$a , $b")
+
+    var name = Pair("Sagar",2358)
+    println(name.first + name.second)
+}
