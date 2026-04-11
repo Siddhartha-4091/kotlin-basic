04c036b939567db8b8edebea7f02172f03e03861
Siddhartha-4091 <gs092502@gmail.com>
2026-04-11 09:40:19 +0530

    multi

--- Oops_Concepts/Inheritance/Animal.kt
+++ Oops_Concepts/Inheritance/Animal.kt
@@ -1,5 +1,22 @@
 package Oops_Concepts.Inheritance
 
-class Multiple_Inheritance {
+open class Animal {
+    fun eat(){
+        println("Animal eat")
+    }
+}open class Dog : Animal(){
+    fun bark(){
+        println("Dog barks")
+    }
+}class Puppy : Dog(){
+    fun weep(){
+        println("Puppy weeps")
+    }
+}
 
+fun main() {
+    val A1=Puppy()
+    A1.weep()
+    A1.bark()
+    A1.eat()
 }
